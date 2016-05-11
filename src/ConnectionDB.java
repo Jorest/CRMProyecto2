ba6d7057675893a3e9b4a15ad2ca85ec7aaa05da
Jorge E. García <jores10@hotmail.com>
2016-05-11 01:13:43 -0600

    prueba coneccion tienen que modificar con su usuario y contrase;a

--- src/ConnectionDB.java
+++ src/ConnectionDB.java
@@ -1,4 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class ConnectionDB {
-
+	public static void main(String args[]) {
+	      Connection c = null;
+	      try {
+	         Class.forName("org.postgresql.Driver");
+	         c = DriverManager
+	            .getConnection("jdbc:postgresql://localhost:5432/Empresa",
+	            "postgres", "none");
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	         System.err.println(e.getClass().getName()+": "+e.getMessage());
+	         System.exit(0);
+	      }
+	      System.out.println("Opened database successfully");
+	   }
 }
