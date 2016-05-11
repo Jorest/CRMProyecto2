341537c5be22e804eb76095eaebc94192fa9415b
Luis <Luis@172.20.11.236>
2016-05-11 11:40:16 -0600

    where top

---
+++ src/CRMProyecto2.java
@@ -0,0 +1,21 @@
+
+import java.sql.Connection;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Luis
+ */
+public class CRMProyecto2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Connection c = ConnectionDB.GetConnection();
+    }
+}
--- src/ConnectionDB.java
+++ src/ConnectionDB.java
@@ -1,4 +1,26 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 //Holi
 public class ConnectionDB {
-
+    
+   public static Connection GetConnection()
+    {
+        Connection c = null;
+      try {
+         Class.forName("org.postgresql.Driver");
+         c = DriverManager
+            .getConnection("jdbc:postgresql://localhost:5432/Proyecto2",
+            "postgres", "postgres");
+          // Statement stmt = c.createStatement();
+        // ResultSet rs = stmt.executeQuery( "SELECT
+      } catch (Exception e) {
+         e.printStackTrace();
+         System.err.println(e.getClass().getName()+": "+e.getMessage());
+         System.exit(0);
+      }
+      System.out.println("Opened database successfully");
+      return c; 
+    }
 }
