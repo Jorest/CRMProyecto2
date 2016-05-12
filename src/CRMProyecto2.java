4d9595490c8bcacd7bae6dc3ea1030b385e5952c
riverkev95 <riv13389@uvg.edu.gt>
2016-05-12 13:10:44 -0600

    Mango Connection

--- src/CRMProyecto2.java
+++ src/CRMProyecto2.java
@@ -1,6 +1,8 @@
 
+import com.mongodb.MongoClient;
 import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.List;
 
 /*
  * To change this template, choose Tools | Templates
@@ -23,6 +25,20 @@ public class CRMProyecto2 {
 
         GUI gui = new GUI();
         gui.setVisible(true);
+        
+        System.out.println("Prueba conexión MongoDB");
+        MongoClient mongo = MangoDB.crearConexion(); 
+        if (mongo != null) {
+        System.out.println("Lista de bases de datos: ");
+            MangoDB.printDatabases(mongo);
+        } else {
+            System.out.println("Error: Conexión no establecida");
+        }
+    }
+ 
+    /**
+     * Clase para crear una conexión a MongoDB.
+     * @return MongoClient conexión
+     */
+}
 
-}
-}
