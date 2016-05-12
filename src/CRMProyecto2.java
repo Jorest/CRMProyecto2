ebc2cfb95a2e413e8646a766e4441fcad15e50c1
Luis <Luis@172.20.11.236>
2016-05-12 17:36:50 -0600

    agrego modulo twitter

--- src/CRMProyecto2.java
+++ src/CRMProyecto2.java
@@ -3,6 +3,9 @@ import com.mongodb.MongoClient;
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import twitter4j.TwitterException;
 
 /*
  * To change this template, choose Tools | Templates
@@ -22,23 +25,32 @@ public class CRMProyecto2 {
 
 
         Connection c = ConnectionDB.GetConnect();
-
-        GUI gui = new GUI();
-        gui.setVisible(true);
-        
-        System.out.println("Prueba conexión MongoDB");
-        MongoClient mongo = MangoDB.crearConexion(); 
-        if (mongo != null) {
-        System.out.println("Lista de bases de datos: ");
-            MangoDB.printDatabases(mongo);
-        } else {
-            System.out.println("Error: Conexión no establecida");
+        Twitter t= new Twitter();
+        try {
+            t.getInformacion("RiverKev95");
+            
+           /** 
+            GUI gui = new GUI();
+            gui.setVisible(true);
+            
+            System.out.println("Prueba conexión MongoDB");
+            MongoClient mongo = MangoDB.crearConexion(); 
+            if (mongo != null) {
+            System.out.println("Lista de bases de datos: ");
+                MangoDB.printDatabases(mongo);
+            } else {
+                System.out.println("Error: Conexión no establecida");
+            }
         }
-    }
- 
-    /**
-     * Clase para crear una conexión a MongoDB.
-     * @return MongoClient conexión
-     */
+     
+        /**
+         * Clase para crear una conexión a MongoDB.
+         * @return MongoClient conexión
+         */
+        } catch (TwitterException ex) {
+            Logger.getLogger(CRMProyecto2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+}
 }
 
